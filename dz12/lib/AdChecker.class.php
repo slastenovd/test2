c427770624fec58cbcb6f1729dff4f7f97a5745a
slastenovd <slastenovd@mail.ru>
2016-01-26 21:58:37 +1000

    dz12 из дома

--- dz12/lib/AdChecker.class.php
+++ dz12/lib/AdChecker.class.php
@@ -4,15 +4,15 @@ class AdChecker { // Сервисный класс - проверяет ad на 
     public static function check($ad) {
         $CheckResult = false;
         if( ($ad instanceof Ads) or ($ad instanceof AdsCompany) or ($ad instanceof AdsPrivatePerson) ){ // Если в качестве параметра передано объявление 
-            if ( ! isset($ad->title) or (isset($ad->title) and ! strlen($ad->title)) ) { // Если значение не приянто, или принято пустое
+            if ( !strlen($ad->getTitle()) ) { // Если значение не приянто, или принято пустое
                 $CheckResult .= 'Не заполнено поле Название объявления<br>';
             }
 
-            if ( ! isset($ad->seller_name) or (isset($ad->seller_name) and ! strlen($ad->seller_name)) ) {  // Если значение не приянто, или принято пустое
+            if ( !strlen($ad->getSeller_name()) )  {  // Если значение не приянто, или принято пустое
                 $CheckResult .= 'Не заполнено поле Ваше имя<br>';
             }
 
-            if ( ! isset($ad->price) or (isset($ad->price) and $ad->price == 0) ) {// Если значение не приянто, или принято пустое
+            if ( $ad->getPrice() == 0 ) {// Если значение не приянто, или принято пустое
                 $CheckResult .= 'Не заполнено поле Цена<br>';
             }
         }
